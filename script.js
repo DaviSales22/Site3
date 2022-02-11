7f9594fb19cae35c769c3c1b71b5c88c08ec609f
Davi <davissales2010@gmail.com>
2022-02-11 20:17:17 -0300

    Add site 1.0

---
+++ script.js
@@ -0,0 +1,4 @@
+function change_text() {
+
+    document.getElementById("demo").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint similique distinctio dicta asperiores at reprehenderit autem saepe iste, perferendis sequi numquam velit officia. Suscipit eos distinctio magni quis qui nisi!";
+}
